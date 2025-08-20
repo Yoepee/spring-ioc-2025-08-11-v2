e73e789fdadb65f760d59449d8b65b2e17709528
Yoepee <kdy2975@naver.com>
2025-08-20 16:41:18 +0900

    refactor: genBean 변경

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,7 +1,7 @@
 package com.ll.framework.ioc;
 
 import com.ll.framework.ioc.annotations.Component;
-import com.ll.standard.util.Ut;
+import lombok.SneakyThrows;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 
@@ -9,64 +9,41 @@ import java.lang.reflect.Constructor;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static com.ll.standard.util.Ut.str.lcfirst;
+
 public class ApplicationContext {
-    private final Reflections reflections;
-    private final Map<String, Class<?>> beanDefinitions = new ConcurrentHashMap<>();
-    private final Map<String, Object> singletons = new ConcurrentHashMap<>();
+    private String basePackage;
+    private static Map<String, Object> singletonObjects = new ConcurrentHashMap<>();
 
     public ApplicationContext(String basePackage) {
-        reflections = new Reflections(basePackage, Scanners.TypesAnnotated, Scanners.SubTypes);
+        this.basePackage = basePackage;
     }
 
     public void init() {
-        // 1) @Component 직붙 클래스 수집
-        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Component.class);
-
-        // 2) 정의된 bean 이름 수집
-        for (Class<?> c : classes) {
-            String name = Ut.str.lcfirst(c.getSimpleName());
-            beanDefinitions.put(name, c);
-        }
+        Reflections reflections = new Reflections(basePackage, Scanners.TypesAnnotated);
+        reflections.getTypesAnnotatedWith(Component.class)
+                .stream()
+                .filter(c -> !c.isAnnotation())
+                .forEach(c -> getBean(lcfirst(c.getSimpleName()), c));
     }
 
-    public <T> T genBean(String beanName) { // 요청하신 메서드명 유지
-        Object exist = singletons.get(beanName);
-        if (exist != null) return (T) exist;
-
-        Class<?> clazz = beanDefinitions.get(beanName);
-        if (clazz == null) throw new NoSuchElementException("No bean named '" + beanName + "'");
-
-        Object created = createBean(beanDefinitions.get(beanName));
-        singletons.put(beanName, created);
-        return (T) created;
+    @SuppressWarnings("unchecked")
+    public static <T> T genBean(String beanName) {
+        return (T) singletonObjects.get(beanName);
     }
 
-    /** 생성자 주입: 가장 파라미터 많은 public 생성자를 골라 타입으로 의존성 주입 */
-    private Object createBean(Class<?> clazz) {
-        try {
-            Constructor<?>[] ctors = clazz.getConstructors();
-            if (ctors.length == 0) {
-                // public 생성자가 없으면 기본 생성자를 시도(비공개 포함)
-                Constructor<?> c = clazz.getDeclaredConstructor();
-                c.setAccessible(true);
-                return c.newInstance();
-            }
+    @SuppressWarnings("unchecked")
+    private <T> T getBean(String beanName, Class<T> cls) {
+        return (T) singletonObjects.computeIfAbsent(beanName, k -> construct(cls));
+    }
 
-            Constructor<?> target = Arrays.stream(ctors)
-                    .max(Comparator.comparingInt(Constructor::getParameterCount))
-                    .orElseThrow();
+    @SneakyThrows
+    private <T> T construct(Class<T> cls) {
+        Constructor<T> constructor = (Constructor<T>) cls.getDeclaredConstructors()[0];
+        Object[] args = Arrays.stream(constructor.getParameterTypes())
+                .map(dep -> getBean(lcfirst(dep.getSimpleName()), dep))
+                .toArray();
 
-            Object[] args = Arrays.stream(target.getParameterTypes())
-                    .map(pt -> Ut.str.lcfirst(pt.getSimpleName()))
-                    .map(this::genBean)
-                    .toArray();
-
-            target.setAccessible(true);
-            return target.newInstance(args);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("No suitable constructor for " + clazz.getName(), e);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create bean: " + clazz.getName(), e);
-        }
+        return constructor.newInstance(args);
     }
 }
